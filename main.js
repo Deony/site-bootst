c5d10f511628125ee8470ad0398a4f00b86086f0
Deony <nostopmicky@gmail.com>
2018-08-17 00:49:17 +0300

    fixed text, media and loop format

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var infoButton = document.querySelectorAll('.infoButton');
 
-[].forEach.call(infoButton, function(el) {
+infoButton.forEach(function(el) {
 	el.addEventListener('click', function(){
 		var submit = document.querySelector('form');
 
@@ -12,7 +12,7 @@ var infoButton = document.querySelectorAll('.infoButton');
 function Validation(event) {
 	event.preventDefault();
 	inputs = document.querySelectorAll('input, textarea');
-	[].forEach.call(inputs, function(el) {
+	inputs.forEach(function(el) {
 		if( !el.value || el.value.length > 32) { //blank or more than 32 symbols
 			el.style.border = '3px solid red';
 		}
